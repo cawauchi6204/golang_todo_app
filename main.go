c6a612978f981ef4695b610c6ac9428b4c46c9d6
cawauchi6204 <cawauchi6204@gmail.com>
2021-01-12 19:59:56 +0900

    18-113完了

--- main.go
+++ main.go
@@ -16,27 +16,30 @@ func main() {
 	*/
 	fmt.Println(models.Db)
 
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.Password = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
+
 	/*
-		u := &models.User{}
-		u.Name = "test"
-		u.Email = "test@example.com"
-		u.Password = "testtest"
+		u, _ := models.GetUser(1)
+
 		fmt.Println(u)
 
-		u.CreateUser()
+		u.Name = "Test2"
+		u.Email = "test2@example.com"
+		u.UpdateUser()
+		u, _ = models.GetUser(1)
+		fmt.Println(u)
+
+		u.DeleteUser()
+		u, _ = models.GetUser(1)
+		fmt.Println(u)
 	*/
 
-	u, _ := models.GetUser(1)
-
-	fmt.Println(u)
-
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
-
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	user, _ := models.GetUser(2)
+	user.CreateTodo("First Todo")
 }
